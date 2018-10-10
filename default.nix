676f345d5c41a627905c3cca0150356a204b4260
Paul Peregud <paulperegud@gmail.com>
2018-10-10 19:08:59 +0200

    enable `escript`

--- default.nix
+++ default.nix
@@ -6,8 +6,9 @@ let
   # I like to define variables for derivations that have
   # a specific version and are subject to change over time.
   elixir = beam.packages.erlangR21.elixir_1_7;
+  # libsecp256k1 = secp256k1-2017-12-18;
 in
 
 mkShell {
-  buildInputs = [ elixir git ];
+  buildInputs = [ elixir git erlangR21 ];
 }
