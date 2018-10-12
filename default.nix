368d3f34beb8913f130f726fb7e7e714f975fca9
Paul Peregud <paulperegud@gmail.com>
2018-10-12 21:20:23 +0200

    use py-solc and py-solc-simple

--- default.nix
+++ default.nix
@@ -7,8 +7,14 @@ let
   # a specific version and are subject to change over time.
   elixir = beam.packages.erlangR21.elixir_1_7;
   libsecp256k1 = secp256k1.secp256k1-2017-12-18;
+  contractbuilding = ps: [
+    ps.requests
+    ps.py-solc
+    ps.py-solc-simple
+  ];
+  pythonForContracts = python3.withPackages contractbuilding;
 in
 
 mkShell {
-  buildInputs = [ elixir git erlangR21 gmp secp256k1 ];
+  propagatedBuildInputs = [ elixir git erlangR21 gmp secp256k1 solc pythonForContracts ];
 }
